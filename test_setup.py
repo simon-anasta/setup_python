2ba2f34b2c885f1e323999babcb516b46461d242
simon-anasta <simon.anastasiadis@gmail.com>
2025-11-22 12:26:08 +1300

    first commit

---
+++ test_setup.py
@@ -0,0 +1,18 @@
+
+# %% imports ------------------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+# %% commands -----------------------------------------------------------------
+print("Everything works!")
+
+var = 3
+
+var2 = var * 2
+
+var3 = var2 ^ 3
+
+
+# %%
