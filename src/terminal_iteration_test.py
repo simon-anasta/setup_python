2ce8d1c97f225cb53caf9980939c511f20edd87e
simon-anasta <simon.anastasiadis@gmail.com>
2026-04-19 15:11:23 +1200

    Exploring GitHub Copilot

---
+++ src/terminal_iteration_test.py
@@ -0,0 +1,17 @@
+# Script to demonstrate terminal iteration
+#
+# Ctrl + `(backtick) opens terminal
+# 
+# Flow:
+# > Edit script
+# > Terminal: `python terminal_iteration_test.py`
+# > repeat
+#
+# Consequences:
+# Run all within editor/IDE,
+# minimising context switching
+#
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
--- src/test_setup.py
+++ src/test_setup.py
@@ -15,4 +15,25 @@ var2 = var * 2
 var3 = var2 ^ 3
 
 
+# %% GitHub Copilot testing ---------------------------------------------------
+# Copilot suggested this functions as auto complete under my guidance.
+
+# function to compute Fibonacci numbers
+
+def fibonacci(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+
+    a, b = 1, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+# function to compute the golden ratio
+
+def golden_ratio(n):
+    return fibonacci(n) / fibonacci(n - 1)
+
 # %%
